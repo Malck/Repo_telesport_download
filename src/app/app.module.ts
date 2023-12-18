6969e9d83d20ea714a6f40a877bed9452b68755c
MC <Malckira@gmail.com>
2023-12-18 11:02:53 +0100

    fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { NgChartsModule } from 'ng2-charts';
+import { CountryComponent } from './pages/country/country.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, NotFoundComponent],
+  declarations: [AppComponent, HomeComponent, CountryComponent, NotFoundComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule,NgChartsModule],
   providers: [],
   bootstrap: [AppComponent],
